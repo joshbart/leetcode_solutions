198859400de88342a4efab12fef7163a2e914ef5
Joshua Bartholomew <joshua.c.bartholomew@gmail.com>
2023-07-23 22:41:00 -0500

    Added solution for 1672

---
+++ python3_richest_customer_wealth_1672/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        most_wealth = 0
+        for cust in range(len(accounts)):
+            cust_wealth = 0
+            for bank in range(len(accounts[cust])):
+                bank_amount = accounts[cust][bank]
+                cust_wealth += bank_amount
+            if cust_wealth > most_wealth:
+                most_wealth = cust_wealth
+        return most_wealth
