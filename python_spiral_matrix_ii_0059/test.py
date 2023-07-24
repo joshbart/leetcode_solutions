817ab81429650a97f8d0730b77b67e74afe07866
Joshua Bartholomew <joshua.c.bartholomew@gmail.com>
2023-07-23 22:10:32 -0500

    Added unit test code.

---
+++ python_spiral_matrix_ii_0059/test.py
@@ -0,0 +1,19 @@
+import unittest
+import solution as s
+
+class TestProblem13(unittest.TestCase):
+
+    def test_case_1(self):
+        data = 3
+        result = s.generateMatrix(data)
+        self.assertEqual(result, [[1,2,3],[8,9,4],[7,6,5]])
+
+    def test_case_2(self):
+        data = 1
+        result = s.generateMatrix(data)
+        self.assertEqual(result, [[1]])
+
+    def test_case_3(self):
+        data = 4
+        result = s.generateMatrix(data)
+        self.assertEqual(result, [[1,2,3,4],[12,13,14,5],[11,16,15,6],[10,9,8,7]])
