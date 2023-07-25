bf0702379e784574fd3f52320b0fe4e844176d65
Joshua Bartholomew <joshua.c.bartholomew@gmail.com>
2023-07-25 13:01:37 -0500

    Wrote the underlying math

--- python3_1342_number_of_steps_to_reduce_a_number_to_zero/solution.py
+++ python3_1342_number_of_steps_to_reduce_a_number_to_zero/solution.py
@@ -1,2 +1,8 @@
 class Solution:
     def numberOfSteps(self, num: int) -> int:
+        while num != 0:
+            if (num % 2) == 0:
+                num = num / 2
+            else:
+                num = num - 1
+        return num
