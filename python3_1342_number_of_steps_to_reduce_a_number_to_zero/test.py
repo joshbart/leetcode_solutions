e475cdf19890b22be2b097d306cf884709b35c14
Joshua Bartholomew <joshua.c.bartholomew@gmail.com>
2023-07-25 12:47:02 -0500

    Added more test cases and the right values for those test cases.

--- python3_1342_number_of_steps_to_reduce_a_number_to_zero/test.py
+++ python3_1342_number_of_steps_to_reduce_a_number_to_zero/test.py
@@ -6,8 +6,20 @@ s = solution.Solution()
 class TestSolution(unittest.TestCase):
 
     def test_case_1(self):
-        data = 0
-        expected_result = 0
+        data = 14
+        expected_result = 6
+        actual_result = s.numberOfSteps(data)
+        self.assertEqual(actual_result, expected_result)
+
+    def test_case_2(self):
+        data = 8
+        expected_result = 4
+        actual_result = s.numberOfSteps(data)
+        self.assertEqual(actual_result, expected_result)
+
+    def test_case_3(self):
+        data = 123
+        expected_result = 12
         actual_result = s.numberOfSteps(data)
         self.assertEqual(actual_result, expected_result)
 
